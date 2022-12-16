9651f3ce29916d38dc8133e2af3f042614311b8c
Your Name <you@example.com>
2022-12-16 06:54:25 +0600

    logo edit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,5 @@
+import { faGlobe, faUniversalAccess } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import CustomLink from '../CustomLink/CustomLink';
@@ -6,7 +8,7 @@ const Header = () => {
     return (
         <div className='grid md:grid-cols-2 bg-green-300 p-3'>
             <div className='text-center'>
-                <Link className='text-3xl font-bold font-mono text-gray-700' to={'/'}>Know The Nation</Link>
+                <Link className='text-3xl font-bold font-mono text-gray-700' to={'/'}><FontAwesomeIcon icon={faGlobe} ></FontAwesomeIcon> Know The Nation</Link>
             </div>
             <div className='flex text-xl md:col-start-2'>
                 <div className="basis-1/4"><CustomLink to={'/'}>Home</CustomLink></div>
